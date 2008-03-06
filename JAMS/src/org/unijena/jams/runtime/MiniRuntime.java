483e73f899f6180aab2bc3928f1bb7add641fa68
Sven Kralisch <sven@kralisch.com>
2008-03-06 13:36:37 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@848 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMS/src/org/unijena/jams/runtime/MiniRuntime.java
+++ JAMS/src/org/unijena/jams/runtime/MiniRuntime.java
@@ -20,7 +20,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  */
-
 package org.unijena.jams.runtime;
 
 import java.awt.BorderLayout;
@@ -28,7 +27,9 @@ import java.util.HashMap;
 import java.util.Observer;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import org.unijena.jams.JAMS;
 import org.unijena.jams.JAMSProperties;
+import org.unijena.jams.JAMSTools;
 import org.unijena.jams.data.JAMSData;
 import org.unijena.jams.model.JAMSComponent;
 import org.unijena.jams.model.JAMSGUIComponent;
@@ -40,9 +41,12 @@ import org.w3c.dom.Document;
  * @author Sven Kralisch
  */
 public class MiniRuntime implements JAMSRuntime {
-    
+
     private JFrame frame;
-    
+    private int debugLevel = JAMS.VVERBOSE;
+    private JAMSLog errorLog = new JAMSLog();
+    private JAMSLog infoLog = new JAMSLog();
+
     /** Creates a new instance of MiniRuntime */
     public MiniRuntime(JAMSComponent component) {
         JAMSModel model = new JAMSModel(this);
@@ -58,96 +62,126 @@ public class MiniRuntime implements JAMSRuntime {
             frame.pack();
             frame.setVisible(true);
         }
-        
-    }
-    
-    public int getDebugLevel() {
-        return -1;
-    }
-    
-    public void setDebugLevel(int aDebugLevel) {
-    }
-    
-    public void println(String s, int debugLevel) {
-        println(s);
-    }
-    
-    public void println(String s) {
-    }
-    
-    public void handle(Exception ex) {
-        ex.printStackTrace();
-    }
-    
-    public HashMap<String, JAMSData> getDataHandles() {
-        return new HashMap<String, JAMSData>();
-    }
-    
-    public void handle(Exception ex, String cName) {
-        handle(ex);
-    }
-    
-    public void handle(Exception ex, boolean proceed) {
-        handle(ex);
-    }
-    
-    public void sendHalt() {
-        System.exit(0);
-    }
-    
-    public void sendHalt(String str) {
-        System.out.print(str);
-        System.exit(0);
-    }
-    
-    public int getRunState() {
-        return -1;
-    }
-    
-    public void addRunStateObserver(Observer o) {}
-    
-    public void addInfoLogObserver(Observer o) {}
-    
-    public void addErrorLogObserver(Observer o) {}
-    
-    public void deleteInfoLogObserver(Observer o) {}
-    
-    public void deleteErrorLogObserver(Observer o) {}
 
-    public String getErrorLog() {
-        return "";
     }
-    
-    public String getInfoLog() {
-        return "";
+
+    public int getDebugLevel() {
+        return this.debugLevel;
+    }
+
+    public void setDebugLevel(int aDebugLevel) {
+        this.debugLevel = aDebugLevel;
+    }
+
+    public void sendInfoMsg(String str) {
+        infoLog.print("INFO: " + str + "\n");
     }
     
     public void sendErrorMsg(String str) {
-        println(str);
+        errorLog.print("ERROR: " + str + "\n");
+    }    
+
+    public void println(String s, int debugLevel) {
+        if (debugLevel <= getDebugLevel()) {
+            sendInfoMsg(s);
+        }
     }
-    
-    public void sendInfoMsg(String str) {
-        println(str);
+
+    public void println(String s) {
+        sendInfoMsg(s);
     }
-    
+
+    public void handle(Exception ex) {
+        handle(ex, null, false);
+    }
+
+    public void handle(Exception ex, String cName) {
+        handle(ex, cName, false);
+    }
+
+    public void handle(Exception ex, boolean proceed) {
+        handle(ex, null, proceed);
+    }
+
+    public void handle(Exception ex, String cName, boolean proceed) {
+
+        String message = "";
+
+        if (cName != null) {
+            message += "Exception occured in component " + cName + "!\n";
+        }
+
+        message += ex.toString();
+        if (getDebugLevel() > JAMS.STANDARD) {
+            message += "\n" + JAMSTools.getStackTraceString(ex.getStackTrace());
+        }
+        sendErrorMsg(message);
+        if (!proceed) {
+            sendHalt();
+        }
+    }
+
+    public HashMap<String, JAMSData> getDataHandles() {
+        return new HashMap<String, JAMSData>();
+    }
+
+    public void sendHalt() {
+        System.exit(0);
+    }
+
+    public void sendHalt(String str) {
+        sendErrorMsg(str);
+        sendHalt();
+    }
+
+    public int getRunState() {
+        return -1;
+    }
+
+    public void addRunStateObserver(Observer o) {
+    }
+
+    public void addInfoLogObserver(Observer o) {
+        infoLog.addObserver(o);
+    }
+
+    public void deleteInfoLogObserver(Observer o) {
+        infoLog.deleteObserver(o);
+    }
+
+    public void addErrorLogObserver(Observer o) {
+        errorLog.addObserver(o);
+    }
+
+    public void deleteErrorLogObserver(Observer o) {
+        errorLog.deleteObserver(o);
+    }
+
+    public String getErrorLog() {
+        return errorLog.getLogString();
+    }
+
+    public String getInfoLog() {
+        return infoLog.getLogString();
+    }
+
     public void addGUIComponent(JAMSGUIComponent component) {
     }
-    
+
     public void initGUI(String title, boolean ontop, int width, int height) {
     }
-    
+
     public JFrame getFrame() {
         return null;
     }
-    
+
     public void runModel() {
     }
-    
+
     public void loadModel(Document modelDocument, JAMSProperties properties) {
     }
-    
+
     public ClassLoader getClassLoader() {
         return ClassLoader.getSystemClassLoader();
     }
-    
 }
