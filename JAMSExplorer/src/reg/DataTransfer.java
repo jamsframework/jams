5faac4fea6177dbb864a46443cdf486c7d051cf7
Heiko Busch <busch_heiko@web.de>
2009-05-06 12:20:50 +0000

    put some meat to it
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@1272 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSExplorer/src/reg/DataTransfer.java
+++ JAMSExplorer/src/reg/DataTransfer.java
@@ -20,16 +20,86 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  */
-
 package reg;
 
 /**
  *
  * @author Sven Kralisch <sven.kralisch at uni-jena.de>
+ *
+ * this class represents one data column, which could be transferred
+ * between subsystems, e.g. in order to append it to a table
  */
 public class DataTransfer {
-    
+
+    /**
+     * the name of the
+     */
+    private String name;
+    /**
+     * the name of the parent (e.g. shape)
+     */
+    private String parentName;
+    /**
+     * some more words except of the name
+     */
+    private String description;
+    /**
+     * the ids
+     */
+    private Object[] ids;
+    /**
+     * the data corresponding to ids
+     */
+    double[] data;
+
     public DataTransfer() {
     }
-    
+
+    public DataTransfer(String name, String parentName, String description, Object[] ids, double[] data) {
+        this.name = name;
+        this.parentName = parentName;
+        this.description = description;
+        this.ids = ids;
+        this.data = data;
+    }
+
+    public double[] getData() {
+        return data;
+    }
+
+    public void setData(double[] data) {
+        this.data = data;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object[] getIds() {
+        return ids;
+    }
+
+    public void setIds(Object[] ids) {
+        this.ids = ids;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
 }
