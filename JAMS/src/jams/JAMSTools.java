fae4ddc75f655182431c7225faaac32d78b2fcf0
Sven Kralisch <sven@kralisch.com>
2009-09-04 13:09:48 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@1407 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMS/src/jams/JAMSTools.java
+++ JAMS/src/jams/JAMSTools.java
@@ -31,9 +31,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
@@ -247,5 +244,4 @@ public class JAMSTools {
         Object data = m.invoke(component);
         return data;
     }
-
 }
