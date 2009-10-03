4bec8e54a28ec33ebafa343be0029f3454d6b4b8
Sven Kralisch <sven@kralisch.com>
2009-10-03 15:07:03 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@1518 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- juice.bat
+++ juice.bat
@@ -3,7 +3,7 @@
 SET ARGUMENTS=
 SET VM=java
 SET OPTIONS=-Xms128M -Xmx512M
-SET LIBS=bin/jams-api.jar;bin/jams-main.jar;bin/jams-common.jar;bin/jams-explorer.jar;bin/jams-ui.jar;lib/jts-1.8.jar
+SET LIBS=bin/jams-api.jar;bin/jams-main.jar;bin/jams-common.jar;bin/jams-explorer.jar;bin/jams-ui.jar
 
 @echo on
 %VM% %OPTIONS% -cp %LIBS% jamsui.juice.JUICE %ARGUMENTS%
