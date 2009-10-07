167be2a2a9babcd6eb1ede8726772036e8ff3b4a
Heiko Busch <busch_heiko@web.de>
2009-10-07 11:45:52 +0000

    KEY_MODEL_MAPPING
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@1568 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSExplorer/src/reg/wizard/tlug/ExplorerWizard.java
+++ JAMSExplorer/src/reg/wizard/tlug/ExplorerWizard.java
@@ -5,10 +5,14 @@
 
 package reg.wizard.tlug;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import org.netbeans.spi.wizard.WizardBranchController;
 import org.netbeans.spi.wizard.WizardPanelProvider;
+import reg.wizard.tlug.panels.BaseDataPanel;
 import reg.wizard.tlug.panels.DataDecisionPanel;
+import reg.wizard.tlug.panels.RegMethodPanel;
 
 
 /**
@@ -17,10 +21,55 @@ import reg.wizard.tlug.panels.DataDecisionPanel;
  */
 public class ExplorerWizard extends WizardBranchController {
 
-    public ExplorerWizard(  ) {
-        super( new InitialStep(  ) );
+    public ExplorerWizard() {
+        super( new InitialStep() );
     }
 
+    /**
+     * mapping of  wizardKey -> key(s) used in model
+     **/
+    public static final Map<String,String[]> KEY_MODEL_MAPPING = createMapping();
+    private static Map<String, String[]> createMapping () {
+        Map<String, String[]> resultMap = new HashMap<String, String[]>();
+
+        resultMap.put(BaseDataPanel.KEY_INTERVAL, new String[] {"J2K.timeInterval"});
+        resultMap.put(RegMethodPanel.KEY_SCHWELLENWERT,
+                new String[] {  "AhumRegionaliser.rsqThreshold",
+                                "TmeanRegionaliser.rsqThreshold",
+                                "SunhRegionaliser.rsqThreshold",
+                                "TminRegionaliser.rsqThreshold",
+                                "TmaxRegionaliser.rsqThreshold",
+                                "WindRegionaliser.rsqThreshold",
+                                "PrecipRegionaliser_1.rsqThreshold",
+                                "PrecipRegionaliser.rsqThreshold"
+                             }
+        );
+        resultMap.put(RegMethodPanel.KEY_GEWICHTUNG,
+                new String[] {  "tmaxWeights.pidw",
+                                "precipWeights.pidw",
+                                "sunhWeights.pidw",
+                                "windWeights.pidw",
+                                "tmeanWeights.pidw",
+                                "tminWeights.pidw",
+                                "ahumWeights.pidw"
+                             }
+        );
+        resultMap.put(RegMethodPanel.KEY_STATION,
+                new String[] {  "AhumRegionaliser.nidw",
+                                "TmeanRegionaliser.nidw",
+                                "SunhRegionaliser.nidw",
+                                "TminRegionaliser.nidw",
+                                "TmaxRegionaliser.nidw",
+                                "WindRegionaliser.nidw",
+                                "PrecipRegionaliser_1.nidw",
+                                "PrecipRegionaliser.nidw"
+                             }
+        );
+
+        return Collections.unmodifiableMap(resultMap);
+    }
+
+    @Override
     protected WizardPanelProvider getPanelProviderForStep(String step, Map collectedData) {
         //There's only one branch point, so we don't need to test the
         //value of step
