55c2018e9d0b497b7ab258417b2213080970cf79
Heiko Busch <busch_heiko@web.de>
2010-01-19 11:49:30 +0000

    some file operations added
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@1751 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSmain/src/jams/tools/JAMSTools.java
+++ JAMSmain/src/jams/tools/JAMSTools.java
@@ -27,12 +27,10 @@ import jams.SystemProperties;
 import jams.model.Component;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -442,4 +440,15 @@ public class JAMSTools {
             }
         }
     }
+
+    /**
+     * delete all given files
+     * @param theFiles
+     */
+    public static void deleteFiles(File[] theFiles) {
+        for (File theFile : theFiles) {
+            theFile.delete();
+        }
+    }
+
 }
