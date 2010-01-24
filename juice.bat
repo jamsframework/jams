eea3b733995163d0e58e9ca2d5b0331b50c6ca5e
Sven Kralisch <sven@kralisch.com>
2010-01-24 20:09:35 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@1773 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- juice.bat
+++ juice.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SET VM=java
-SET OPTIONS=-Xms128M -Xmx512M -Dsun.java2d.d3d=false -Djava.library.path=bin/win64
+SET OPTIONS=-Xms128M -Xmx512M -Dsun.java2d.d3d=false -Djava.library.path=bin/win32
 
 @echo on
 %VM% %OPTIONS% -jar lib/juice-starter.jar %1 %2 %3 %4
