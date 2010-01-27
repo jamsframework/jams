8bb350d07bcf0928480ff5d0c36859825d214d4b
Heiko Busch <busch_heiko@web.de>
2010-01-27 13:36:31 +0000

    some helpers added
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@1782 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSmain/src/jams/io/XMLProcessor.java
+++ JAMSmain/src/jams/io/XMLProcessor.java
@@ -20,7 +20,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  *
  */
-
 package jams.io;
 
 import java.io.*;
@@ -34,98 +33,113 @@ import org.w3c.dom.NodeList;
  * @author S. Kralisch
  */
 public class XMLProcessor {
-    
+
     static String[] regex = {"jamsvar class=\".*\" name", "compoundcomponent", "jamsvar", "providervar", "spatattrib", "provider=", "org\\.unijena\\.jams\\.gui", "org\\.unijena\\.jamscomponents", "org\\.unijena\\.jams\\.", "jams\\.model\\.cc\\."};
     static String[] replace = {"jamsvar name", "contextcomponent", "var", "cvar", "attribute", "context=", "jams.components.gui", "jams.components", "jams.", "jams.model."};
-/*    static String[] regex = {"jamsvar class=\".*\" "};
+    /*    static String[] regex = {"jamsvar class=\".*\" "};
     static String[] replace = {"jamsvar "};
- */
-    
+     */
+
     /** Creates a new instance of XMLProcessor */
     public XMLProcessor() {
     }
-    
+
     public static String modelDocConverter(String inFileName) {
-        
+
         String outFileName = inFileName;
-        
+
         try {
-            
+
             BufferedReader reader = new BufferedReader(new FileReader(inFileName));
-            
+
             String s, newDoc = "", oldDoc;
-            
+
             while ((s = reader.readLine()) != null) {
                 newDoc += s + "\n";
             }
-            newDoc = newDoc.substring(0, newDoc.length()-1);
+            newDoc = newDoc.substring(0, newDoc.length() - 1);
             oldDoc = newDoc;
-            
+
             for (int i = 0; i < regex.length; i++) {
                 newDoc = newDoc.replaceAll(regex[i], replace[i]);
             }
-            
+
             if (!newDoc.contentEquals(oldDoc)) {
                 File f = new File(inFileName);
                 String fName = "_" + f.getCanonicalFile().getName();
                 String pName = f.getParent();
-                
+
                 outFileName = pName + File.separator + fName;
-                
+
                 BufferedWriter writer = new BufferedWriter(new FileWriter(outFileName));
                 writer.write(newDoc);
                 writer.close();
             }
-            
-            
+
+
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-        
+
         return outFileName;
     }
 
-        public static Node getModelNode(Node root){
-        if (root.getNodeName().equals("model")){
+    /**
+     * get node of model
+     * @param root
+     * @return node with attributeName "model"
+     */
+    public static Node getModelNode(Node root) {
+        if (root.getNodeName().equals("model")) {
             return root;
         }
         NodeList childs = root.getChildNodes();
-        for (int i=0;i<childs.getLength();i++){
+        for (int i = 0; i < childs.getLength(); i++) {
             Node model = getModelNode(childs.item(i));
-            if (model != null)
+            if (model != null) {
                 return model;
+            }
         }
         return null;
     }
 
-    public static String getWorkspacePath(Document model){
+    /**
+     * get workspace path from model
+     * @param model
+     * @return value of node with attributeName "workspaceDirectory"
+     */
+    public static String getWorkspacePath(Document model) {
         Element root = model.getDocumentElement();
-        Element modelElem = (Element)XMLProcessor.getModelNode(root);
-        //modelNode.g
+        Element modelElem = (Element) XMLProcessor.getModelNode(root);
 
         NodeList childs = modelElem.getChildNodes();
-        for (int i=0;i<childs.getLength();i++){
+        for (int i = 0; i < childs.getLength(); i++) {
             Node node = childs.item(i);
-            if (node.getNodeName().equals("var")){
-                if ( ((Element)node).getAttribute("name").equals("workspaceDirectory") ){
-                    return ((Element)node).getAttribute("value");
+            if (node.getNodeName().equals("var")) {
+                if (((Element) node).getAttribute("name").equals("workspaceDirectory")) {
+                    return ((Element) node).getAttribute("value");
                 }
             }
         }
         return null;
     }
 
-    public static boolean setWorkspacePath(Document model, String theWorkSpacePath){
+    /**
+     * set workspace path to model
+     * @param model
+     * @param theWorkSpacePath
+     * @return true, if node with attributeName "workspaceDirectory" could be found, otherwise false
+     */
+    public static boolean setWorkspacePath(Document model, String theWorkSpacePath) {
         Element root = model.getDocumentElement();
-        Element modelElem = (Element)XMLProcessor.getModelNode(root);
-        //modelNode.g
+        Element modelElem = (Element) XMLProcessor.getModelNode(root);
 
         NodeList childs = modelElem.getChildNodes();
-        for (int i=0;i<childs.getLength();i++){
+        for (int i = 0; i < childs.getLength(); i++) {
             Node node = childs.item(i);
-            if (node.getNodeName().equals("var")){
-                if ( ((Element)node).getAttribute("name").equals("workspaceDirectory") ){
-                    ((Element)node).setAttribute("value", theWorkSpacePath);
+            if (node.getNodeName().equals("var")) {
+                if (((Element) node).getAttribute("name").equals("workspaceDirectory")) {
+                    ((Element) node).setAttribute("value", theWorkSpacePath);
                     return true;
                 }
             }
@@ -133,34 +147,60 @@ public class XMLProcessor {
         return false;
     }
 
-    static public Node getFirstComponent(Node root){
+    /**
+     * get the first component of root node
+     * @param root - the root node
+     * @return first node with attributeName "component" or "contextcomponent"
+     */
+    static public Node getFirstComponent(Node root) {
         NodeList childs = root.getChildNodes();
-        for (int i=0;i<childs.getLength();i++){
-            if ( childs.item(i).getNodeName().equals("component") || childs.item(i).getNodeName().equals("contextcomponent") ){
+        for (int i = 0; i < childs.getLength(); i++) {
+            if (childs.item(i).getNodeName().equals("component") || childs.item(i).getNodeName().equals("contextcomponent")) {
                 return childs.item(i);
             }
             Node result = getFirstComponent(childs.item(i));
-            if (result != null)
+            if (result != null) {
                 return result;
-        }
-        return null;
-    }
-    static public Node findComponentNode(Node context, String name){
-        NodeList childs = context.getChildNodes();
-        for (int i=0;i<childs.getLength();i++){
-            Node node = childs.item(i);
-            String node_name = node.getNodeName();
-            if (node_name.equals("component") || node_name.equals("contextcomponent") || node_name.equals("model")){
-                if (((Element)node).getAttribute("name").equals(name)){
-                    return node;
-                }
             }
-            Node result = findComponentNode(node,name);
-            if (result != null)
-                return result;
         }
         return null;
     }
 
-    
+    /**
+     * find the appropriate node within context
+     * @param context
+     * @param attributeName
+     * @return Node if attribute "attributeName"  = attributeName or null
+     */
+    static public Node findComponentNode(Node context, String attributeName) {
+        NodeList childs = context.getChildNodes();
+        for (int i = 0; i < childs.getLength(); i++) {
+            Node node = childs.item(i);
+            String node_name = node.getNodeName();
+            if (node_name.equals("component") || node_name.equals("contextcomponent") || node_name.equals("model")) {
+                if (((Element) node).getAttribute("name").equals(attributeName)) {
+                    return node;
+                }
+            }
+            Node result = findComponentNode(node, attributeName);
+            if (result != null) {
+                return result;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * get the value of a certain node
+     * @param context
+     * @param attributeName
+     * @return value if attribute "attributeName"  = attributeName or null
+     */
+    public static String getComponentNodeValue(Node context, String attributeName) {
+        Node componentNode = findComponentNode(context, attributeName);
+        if (componentNode != null) {
+            return ((Element)componentNode).getAttribute("value");
+        }
+        return null;
+    }
 }
