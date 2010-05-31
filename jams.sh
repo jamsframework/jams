04dcd0491f151a9b008cce335029d7c407d3c387
Sven Kralisch <sven@kralisch.com>
2010-05-31 10:11:09 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2009 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- jams.sh
+++ jams.sh
@@ -2,6 +2,6 @@
 
 PLATFORM=linux64
 VM=java
-OPTIONS="-Xms128M -Xmx512M -Dsun.java2d.d3d=false -Djava.library.path=bin/$PLATFORM"
+OPTIONS="-Xms128M -Xmx512M -Dsun.java2d.d3d=false -Djava.library.path=bin/$PLATFORM -splash:"
 $VM $OPTIONS -jar lib/jams-starter.jar $1 $2 $3 $4
 
--- juice.bat
+++ juice.bat
@@ -2,7 +2,7 @@
 
 SET platform=win64
 SET VM=java
-SET OPTIONS=-Xms128M -Xmx512M -XX:MaxPermSize=128m -Dsun.java2d.d3d=false -Djava.library.path=bin/%platform%
+SET OPTIONS=-Xms128M -Xmx512M -XX:MaxPermSize=128m -Dsun.java2d.d3d=false -Djava.library.path=bin/%platform% -splash:
 
 @echo on
 %VM% %OPTIONS% -jar lib/juice-starter.jar %1 %2 %3 %4 %5 %6
