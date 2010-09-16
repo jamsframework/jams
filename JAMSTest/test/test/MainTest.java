cdc347077dab0d730faba8ef6b252f0502c48f52
Christian Fischer <christian.fischer.2@uni-jena.de>
2010-09-16 15:52:03 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2088 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

---
+++ JAMSTest/test/test/MainTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+import jams.test.J2KFileComparator;
+import jams.test.J2KFileComparator.Report;
+import java.io.File;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author chris
+ */
+public class MainTest {
+
+    public MainTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    public void testJ2KGehlberg(){
+        System.out.println("performing JAMS - Gehlberg Test");
+
+        System.out.println(System.getProperty("user.dir"));
+        String args[] = new String[]{
+            "-c", "../test.jap",
+            "-m", "../../../modeldata/JAMS-Gehlberg/j2k_gehlberg.jam"
+        };
+        jamsui.launcher.JAMSui.main(args);
+
+        //test output
+        try{
+            Report report = J2KFileComparator.compare(
+                    new File("../../../modeldata/JAMS-Gehlberg/output/current/TimeLoop.dat"),
+                    new File("../../../modeldata/JAMS-Gehlberg/test/reference/TimeLoop.dat"),0.001);
+            report.print(System.out);
+            org.junit.Assert.assertFalse(report.isReportEmpty());
+        }catch(Exception e){
+            org.junit.Assert.fail("Exception while comparing Gehlberg - Results:" + e.toString());
+        }
+    }
+
+     public void testCalibration(){
+        System.out.println("performing Calibration Test");
+
+        System.out.println(System.getProperty("user.dir"));
+        String args[] = new String[]{
+            "-c", "../test.jap",
+            "-m", "../../../modeldata/Calibration/dixon1.jam"
+        };
+        jamsui.launcher.JAMSui.main(args);
+
+        //test output
+        try{
+            Report report = J2KFileComparator.compare(
+                    new File("../../../modeldata/calibration/output/current/result.dat"),
+                    new File("../../../modeldata/calibration/test/reference/reference.dat"),0.001);
+            report.print(System.out);
+            org.junit.Assert.assertFalse(report.isReportEmpty());
+        }catch(Exception e){
+            org.junit.Assert.fail("Exception while comparing Calibration - Results:" + e.toString());
+        }
+     }
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    public void testMain() {
+        testJ2KGehlberg();
+        testCalibration();
+    }
+
+}
