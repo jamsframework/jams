282a9c9379c1f78a85df5e28d497b51b5999b73f
Sven Kralisch <sven@kralisch.com>
2011-02-23 14:27:12 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2217 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

---
+++ JAMSmain/src/jams/JAMSExceptionHandler.java
@@ -0,0 +1,36 @@
+/*
+ * JAMSExceptionHandler.java
+ * Created on 22.02.2011, 11:48:39
+ *
+ * This file is part of JAMS
+ * Copyright (C) FSU Jena
+ *
+ * JAMS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * JAMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JAMS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package jams;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sven Kralisch <sven.kralisch at uni-jena.de>
+ */
+public interface JAMSExceptionHandler {
+
+    public void handle(JAMSException ex);
+    public void handle(ArrayList<JAMSException> exList);
+
+}
