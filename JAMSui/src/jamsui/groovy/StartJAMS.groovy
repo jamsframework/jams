996134c70a18d8e4bf77deaabd95c8542f80dc3f
Sven Kralisch <sven@kralisch.com>
2012-01-13 05:51:08 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2365 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSui/src/jamsui/groovy/StartJAMS.groovy
+++ JAMSui/src/jamsui/groovy/StartJAMS.groovy
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2012 Sven Kralisch <sven.kralisch at uni-jena.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package jamsui.groovy
@@ -14,7 +26,7 @@ import jams.meta.*
 import jams.io.*
 import jams.model.*
 
-// class needed lateron, could also use null instead..
+// helper class needed later on, could also use null instead..
 class ExHandler implements ExceptionHandler {
     
     public void handle(JAMSException ex) {
