8a4801417c31feefacf0f416c8e1f34ea370be3a
Christian Fischer <christian.fischer.2@uni-jena.de>
2012-02-24 08:42:40 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2421 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMScomponents/src/jams/components/io/StandardEntityReader.java
+++ JAMScomponents/src/jams/components/io/StandardEntityReader.java
@@ -6,6 +6,16 @@
  * Copyright (C) 2005 S. Kralisch and P. Krause
  *
  * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This file is part of JAMS
+ * Copyright (C) 2005 FSU Jena
+ *
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
