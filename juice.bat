79cd5901d02050cc581b4a8352e6122d4e12ba27
Sven Kralisch <sven@kralisch.com>
2013-01-18 14:02:23 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2576 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- juice.bat
+++ juice.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET platform=win32
+SET platform=win64
 SET VM=java
 SET OPTIONS=-Xms128M -Xmx512M -XX:MaxPermSize=128m -Dsun.java2d.d3d=false -Djava.library.path=bin/%platform% -splash:
 
