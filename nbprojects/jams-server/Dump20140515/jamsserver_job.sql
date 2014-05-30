974b0a25f10e110fd00d0e30657a8d4b8e0e6f2a
Sven Kralisch <sven@kralisch.com>
2014-05-30 08:56:13 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2918 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- nbprojects/jams-server/Dump20140515/jamsserver_job.sql
+++ nbprojects/jams-server/Dump20140515/jamsserver_job.sql
@@ -31,6 +31,7 @@ CREATE TABLE `job` (
   `server` varchar(500) DEFAULT NULL,
   `pid` int(11) DEFAULT NULL,
   `startTime` datetime DEFAULT NULL,
+  `modelFileID` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
