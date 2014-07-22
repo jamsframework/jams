7b6a0b6574e5540b9f7806905e143777f151241f
Sven Kralisch <sven@kralisch.com>
2014-07-22 19:58:44 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@2979 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- jams.bat
+++ jams.bat
@@ -1,8 +1,7 @@
 @echo off
 
-SET platform=win32
 SET VM=java
-SET OPTIONS=-Xms128M -Xmx512M -XX:MaxPermSize=128m -Dsun.java2d.d3d=false -Djava.library.path=bin/%platform% -splash:
+SET OPTIONS=-Xms128M -Xmx512M -Dsun.java2d.d3d=false -splash:
 
 @echo on
 %VM% %OPTIONS% -jar lib/jams-starter.jar %*
