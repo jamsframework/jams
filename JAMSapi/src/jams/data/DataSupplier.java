74e6e8931c85af3b23effdf91cecb4e54f6a3689
Christian Fischer <christian.fischer.2@uni-jena.de>
2014-10-16 07:58:24 +0000

    added a simple DataSupplier interface
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3061 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

---
+++ JAMSapi/src/jams/data/DataSupplier.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jams.data;
+
+/**
+ *
+ * @author christian
+ */
+public interface DataSupplier<T> extends Iterable<T>{
+    abstract public int size();
+    abstract T get(int i);
+}
