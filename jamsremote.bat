c62837a275d64f2e591c42d7a1e4ebd269d2aaf7
Sven Kralisch <sven@kralisch.com>
2015-02-20 08:37:27 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3139 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- jamsremote.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-SET platform=win32
-SET VM=java
-SET OPTIONS=-Xms128M -Xmx512M -XX:MaxPermSize=128m -Dsun.java2d.d3d=false -Djava.library.path=bin/%platform% -splash:
-
-@echo on
-%VM% %OPTIONS% -jar lib/jamsremote-starter.jar %*
