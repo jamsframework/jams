d5640484585878a738ccdf1483bee4b3f0218333
Christian Siegert <christian.siegert@uni-jena.de>
2017-02-28 18:52:07 +0000

    Added server load to Jobs page.
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3312 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSWebClient/src/config/index.js
+++ JAMSWebClient/src/config/index.js
@@ -2,5 +2,8 @@ export default {
 	baseUrl: "http://localhost:8080/jamscloud/webresources",
 
 	// Interval (in milliseconds) to check connection to JAMS Cloud Server
-	connectionInterval: 30 * 1000
+	connectionInterval: 30 * 1000,
+
+	// Interval (in milliseconds) to check server load
+	serverLoadInterval: 10 * 1000
 };
--- JAMSWebClient/src/views/jobs/list/list.js
+++ JAMSWebClient/src/views/jobs/list/list.js
@@ -2,26 +2,49 @@ import config from "../../../config";
 import date from "../../../date";
 
 export default {
+	beforeDestroy() {
+		clearInterval(this.intervalId);
+	},
 	data() {
 		return {
-			jobs: []
+			intervalId: 0,
+			jobs: [],
+			serverLoad: -1
 		};
 	},
 	methods: {
-		formatDate: date.formatDate
+		formatDate: date.formatDate,
+		getJobs() {
+			const url = config.baseUrl + "/job/find";
+
+			this.$http.get(url).then((response) => {
+				response.json().then((data) => {
+					this.jobs = data.jobs;
+				}, (response) => {
+					console.error("jobs: Parsing JSON response failed:", response);
+				});
+			}, (response) => {
+				console.error("jobs: Unexpected response:", response);
+			});
+		},
+		getServerLoad() {
+			const url = config.baseUrl + "/job/load";
+
+			this.$http.get(url).then((response) => {
+				response.json().then((data) => {
+					this.serverLoad = data;
+				}, (response) => {
+					console.error("jobs: Parsing JSON response failed:", response);
+				});
+			}, (response) => {
+				console.error("jobs: Unexpected response:", response);
+			});
+		}
 	},
 	mounted() {
-		const url = config.baseUrl + "/job/find";
+		this.getJobs();
+		this.getServerLoad();
 
-		this.$http.get(url).then((response) => {
-			response.json().then((data) => {
-				console.debug(response.data);
-				this.jobs = data.jobs;
-			}, (response) => {
-				console.error("Jobs: Parsing JSON response failed:", response);
-			});
-		}, (response) => {
-			console.error("Jobs: Unexpected response:", response);
-		});
+		this.intervalId = setInterval(this.getServerLoad, config.serverLoadInterval);
 	}
 };
