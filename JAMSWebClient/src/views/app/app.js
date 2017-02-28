c4dfc3c4174bd965ae251816941e02873814fca0
Christian Siegert <christian.siegert@uni-jena.de>
2017-02-28 18:52:06 +0000

    Connection status is updated whenever a request is made.
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3310 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSWebClient/src/views/app/app.js
+++ JAMSWebClient/src/views/app/app.js
@@ -4,14 +4,23 @@ import config from "../../config";
 
 export default {
 	beforeCreate() {
+		// Allow cookies for requests to API (for cross-origin requests)
 		Vue.http.options.credentials = true;
+
+		// Intercept responses
+		Vue.http.interceptors.push(function(request, next) {
+			next(function(response) {
+				this.$store.commit("setIsConnected", response.status !== 0);
+			});
+		});
 	},
 	created() {
-		this.connect();
-		setInterval(this.connect, config.connectionInterval);
+		this.getVersion();
+		setInterval(this.getVersion, config.connectionInterval);
 	},
 	methods: {
-		connect() {
+		// Get JAMS Cloud Server version
+		getVersion() {
 			const url = config.baseUrl + "/version";
 
 			const options = {
@@ -25,12 +34,9 @@ export default {
 					this.$store.commit("setIsConnected", true);
 					this.$store.commit("setJamsCloudServerVersion", data);
 				}, (response) => {
-					console.error("app: Parsing JSON response failed:", response);
+					console.error("app: Parsing text response failed:", response);
 				});
 			}, (response) => {
-				if (response.status === 0) {
-					this.$store.commit("setIsConnected", false);
-				}
 				console.error("app: Unexpected response:", response);
 			});
 		}
