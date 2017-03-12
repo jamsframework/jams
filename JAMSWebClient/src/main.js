f052f61c95587130b3326aaab501d34e88d7bc50
Christian Siegert <christian.siegert@uni-jena.de>
2017-03-12 15:44:05 +0000

    HTTP responses with status code 403 Forbidden trigger a status check whether the user is signed in, and if she is not, redirect her to the sign-in page.
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3346 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSWebClient/src/main.js
+++ JAMSWebClient/src/main.js
@@ -4,6 +4,7 @@ import VueResource from "vue-resource";
 import {sync} from "vuex-router-sync";
 
 import app from "./views/app/app.vue";
+import config from "./config";
 import router from "./router/index.js";
 import store from "./store/index.js";
 
@@ -25,12 +26,12 @@ Vue.http.interceptors.push(function(request, next) {
 
 		switch (response.status) {
 			case 0:
+				handle403();
+				break;
 			case 200:
 				break;
 			case 403:
-				console.debug("Intercepted 403. Should redirect!");
-				// Vue.router.push("/sign-in");
-				// console.debug("Intercepted 403. Should have redirected!");
+				handle403();
 				break;
 			case 409:
 				break;
@@ -39,6 +40,63 @@ Vue.http.interceptors.push(function(request, next) {
 		}
 	});
 });
+
+/* eslint-disable no-unused-vars */
+let isHandling403 = false;
+
+// handle403 handles HTTP status code 403 Forbidden responses. If the user is
+// signed in locally, it is checked whether he is still signed in on the server.
+// If he is not, he is redirected to the sign-in page.
+function handle403() {
+	if (isHandling403) {
+		return;
+	}
+
+	isHandling403 = true;
+
+	if (!store.state.user.isSignedIn) {
+		router.push({
+			path: "/sign-in",
+			query: {
+				from: router.currentRoute.fullPath
+			}
+		});
+		isHandling403 = false;
+		return;
+	}
+
+	const url = config.baseUrl + "/user/isConnected";
+
+	const options = {
+		credentials: true
+	};
+
+	Vue.http.get(url, options).then((response) => {
+		response.json().then((data) => {
+			// If user is signed in on server, the 403 response must mean the
+			// action is forbidden even for signed in users
+			if (data === true) {
+				isHandling403 = false;
+				return;
+			}
+
+			// Sign out locally
+			store.commit("user/signOut");
+
+			// Redirect to sign-in page
+			router.push({
+				path: "/sign-in",
+				query: {
+					from: router.currentRoute.fullPath
+				}
+			});
+			isHandling403 = false;
+		}, (response) => {
+			console.error("main: Parsing JSON response failed:", response);
+			isHandling403 = false;
+		});
+	});
+}
 
 /* eslint-disable no-new */
 new Vue({
--- JAMSWebClient/src/router/index.js
+++ JAMSWebClient/src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from "vue-router";
 
 import account from "../views/account/account/account.vue";
 import accountPassword from "../views/account/password/password.vue";
-// import config from "../config";
 import jobsList from "../views/jobs/list/list.vue";
 import jobsShow from "../views/jobs/show/show.vue";
 import notFound from "../views/not-found.vue";
@@ -81,43 +80,6 @@ const router = new VueRouter({
 
 // Check rules before navigating to a page
 router.beforeEach((to, from, next) => {
-	// // Query the server whether the user is signed in
-	// if (!store.state.user.queriedIsConnected) {
-	// 	const url = config.baseUrl + "/user/isConnected";
-	//
-	// 	const options = {
-	// 		credentials: true
-	// 	};
-	//
-	// 	Vue.http.get(url, options).then((response) => {
-	// 		response.json().then((data) => {
-	// 			store.commit("user/queriedIsConnected");
-	//
-	// 			// If user is signed in on server, update sign in status in app
-	// 			if (data === true) {
-	// 				store.commit("user/signIn");
-	// 				next();
-	// 				return;
-	// 			}
-	//
-	// 			// If user is not signed in on server, redirect to sign-in page
-	// 			next({
-	// 				path: "/sign-in",
-	// 				query: {
-	// 					from: to.path
-	// 				}
-	// 			});
-	// 		}, (response) => {
-	// 			console.error("router: Parsing JSON response failed:", response);
-	// 		});
-	// 	});
-	//
-	// 	// Block navigation while request is running
-	// 	next(false);
-	//
-	// 	return;
-	// }
-
 	// If page or any parent page requires authentication, redirect to sign-in page
 	if (to.matched.some((record) => record.meta.requiresAuth) && !store.state.user.isSignedIn) {
 		next({
