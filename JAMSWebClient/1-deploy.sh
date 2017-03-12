79cb8f3b75c7996a2a2727cb44cd517ab34901df
Christian Siegert <christian.siegert@uni-jena.de>
2017-03-12 15:44:13 +0000

    Added scripts for upload to and deployment on production server.
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3355 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

---
+++ JAMSWebClient/1-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 1-deploy.sh is the first step in deploying the client. It is supposed to be
+# run on the development machine. The script generates a static website and
+# uploads the files to the server. Make sure the Node.js dev server is not
+# running. Tested on macOS.
+
+# Go to project root directory
+DIR=~/Dev/Projects/JAMSWebClient;
+cd "$DIR";
+
+# Build static website
+mv ./dist ~/.Trash/;
+mkdir ./dist;
+npm run build;
+
+# Compress files
+tar -c -J --options xz:9 -v ./dist > ./dist.tar.xz
+
+# Upload to server
+scp ./dist.tar.xz worf:~/websites/jams-web-client/
+
+# Delete archive
+mv ./dist.tar.xz ~/.Trash
+
+echo "==> SSH into the server and execute '/home/modis/websites/jams-web-client/2-deploy.sh' there."
---
+++ JAMSWebClient/2-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 2-deploy.sh is the second step in deploying the client. It is supposed to be
+# run on the server that hosts the website. The script unpacks the uploaded
+# archive and moves it into the correct location.
+
+DIR=~/websites/jams-web-client
+cd "$DIR"
+
+# Unpack archive to temporary location
+mkdir ./temp
+tar -xf ./dist.tar.xz -C ./temp
+
+# Move current website to backup location
+mv ./dist ./dist.bak
+
+# Move new website to live location
+mv ./temp/dist ./dist
+
+# Clean up
+rmdir ./temp
+rm ./dist.tar.xz
+rm -r ./dist.bak
