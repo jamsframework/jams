4e17a43c08826e65139d4b00653885c75b117678
Christian Siegert <christian.siegert@uni-jena.de>
2017-03-17 20:54:11 +0000

    Updated config to use correct API URL.
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3361 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMSWebClient/src/config/index.js
+++ JAMSWebClient/src/config/index.js
@@ -1,6 +1,6 @@
 export default {
 	apiBaseUrl: process.env.NODE_ENV === "production"
-		? "http://worf.geogr.uni-jena.de:8080/jamscloud/webresources"
+		? "http://worf.geogr.uni-jena.de:80/jamscloud/webresources"
 		: "http://localhost:8080/jamscloud/webresources",
 
 	// Interval (in milliseconds) to check connection to JAMS Cloud Server
