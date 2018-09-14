8e655536123d22412c9964f383487ba184300254
Sven Kralisch <sven@kralisch.com>
2018-09-14 15:11:37 +0000

    added new components
    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3455 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMScomponents/src/jams/components/core/FileInputContext.java
+++ JAMScomponents/src/jams/components/core/FileInputContext.java
@@ -1,71 +1,90 @@
 /*
- * TemporalContext.java
- * Created on 31. Juli 2005, 20:24
+ * FileInputContext.java
+ * Created on 27.07.2017, 13:36:19
  *
  * This file is part of JAMS
- * Copyright (C) 2005 FSU Jena
+ * Copyright (C) FSU Jena
  *
- * This program is free software; you can redistribute it and/or
+ * JAMS is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * JAMS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * along with JAMS. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 package jams.components.core;
 
-import jams.model.*;
-import jams.workspace.stores.OutputDataStore;
 import jams.JAMS;
 import jams.data.*;
 import jams.dataaccess.DataAccessor;
-import jams.io.datatracer.DataTracer;
+import jams.io.BufferedFileReader;
 import jams.io.datatracer.AbstractTracer;
+import jams.io.datatracer.DataTracer;
+import jams.model.*;
 import jams.workspace.stores.Filter;
+import jams.workspace.stores.OutputDataStore;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  *
- * @author S. Kralisch
+ * @author Sven Kralisch <sven.kralisch at uni-jena.de>
  */
-@JAMSComponentDescription(title = "Temporal context",
+@JAMSComponentDescription(
+        title = "FileInputContext",
         author = "Sven Kralisch",
-        date = "2005-07-31",
-        version = "1.0_1",
-        description = "This component represents a JAMS context which can be used to "
-        + "represent iteration over discrete time steps typically used in conceptional"
-        + "environmental models")
+        description = "Context that iterates over lines in a tab-separated "
+        + "text file and offers the stored data in different "
+        + "columns (attribute \"values\").",
+        date = "2018-09-14",
+        version = "1.0_1"
+)
 @VersionComments(entries = {
-    @VersionComments.Entry(version = "1.0_0", date = "2005-07-31", comment = "Initial Version"),
-    @VersionComments.Entry(version = "1.1_0", date = "2015-03-16", comment = "Added time step output")
+    @VersionComments.Entry(version = "1.0_0", comment = "Initial version"),
+    @VersionComments.Entry(version = "1.0_1", date = "2018-09-14", comment = "Some improvements")
 })
-public class TemporalContext extends JAMSContext {
+public class FileInputContext extends JAMSContext {
 
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-            description = "Time interval of temporal context")
-    public Attribute.TimeInterval timeInterval;
+    /*
+     *  Component attributes
+     */
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "The name of the file to read from"
+    )
+    public Attribute.String fileName;
 
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-            description = "Current date of temporal context")
-    public Attribute.Calendar current;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "The key word that indicates the start of the data section",
+            defaultValue = "@start"
+    )
+    public Attribute.String startIndicator;
 
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-            description = "Print the current time every \"printTime\" time steps",
-            defaultValue = "0")
-    public Attribute.Integer printTime;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "Values that were read from file"
+    )
+    public Attribute.Double[] values;
 
-    private Attribute.Calendar lastValue;
-    private int counter = 0;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "Current iteration number"
+    )
+    public Attribute.Double counter;
 
-    public TemporalContext() {
-        super();
-    }
+    transient private BufferedFileReader fileReader;
+    private String line;
+    private int counter_;
 
     @Override
     protected DataTracer createDataTracer(OutputDataStore store) {
@@ -75,7 +94,7 @@ public class TemporalContext extends JAMSContext {
             public void trace() {
                 // check for filters on other contexts first
                 for (Filter filter : store.getFilters()) {
-                    if (filter.getContext() != TemporalContext.this) {
+                    if (filter.getContext() != FileInputContext.this) {
                         String s = filter.getContext().getTraceMark();
                         //Matcher matcher = filter.getPattern().matcher(s);
                         if (!filter.isFiltered(s)) {
@@ -88,7 +107,7 @@ public class TemporalContext extends JAMSContext {
 
                 // take care of filters in this context
                 for (Filter filter : store.getFilters()) {
-                    if (filter.getContext() == TemporalContext.this) {
+                    if (filter.getContext() == FileInputContext.this) {
                         //Matcher matcher = filter.getPattern().matcher(traceMark);
                         if (!filter.isFiltered(traceMark)) {
                             return;
@@ -112,21 +131,13 @@ public class TemporalContext extends JAMSContext {
         };
     }
 
+    /*
+     *  Component run stages
+     */
     @Override
     public void init() {
         super.init();
-        if (timeInterval == null) {
-            getModel().getRuntime().sendErrorMsg(JAMS.i18n("time_interval_for_JAMS_Temporal_context_not_provided"));
-        }
-        if (timeInterval.getEnd() == null || timeInterval.getStart() == null) {
-            getModel().getRuntime().sendErrorMsg(JAMS.i18n("time_interval_for_JAMS_Temporal_context_not_provided"));
-        }
-        lastValue = timeInterval.getEnd().clone();
-        lastValue.add(timeInterval.getTimeUnit(), -timeInterval.getTimeUnitCount());
-        lastValue.add(Attribute.Calendar.MILLISECOND, 1);
-        if (current == null) {
-            current = this.getModel().getRuntime().getDataFactory().createCalendar();
-        }
+        initFile();
     }
 
     @Override
@@ -139,6 +150,31 @@ public class TemporalContext extends JAMSContext {
         super.cleanupAll();
     }
 
+    private void initFile() {
+        try {
+            fileReader = new BufferedFileReader(new FileInputStream(new File(this.getModel().getWorkspacePath(), fileName.getValue())), JAMS.getCharset());
+            while ((line = fileReader.readLine()) != null) {
+                if (line.startsWith(startIndicator.getValue())) {
+                    break;
+                }
+            }
+            line = fileReader.readLine();
+            int i = 0;
+            boolean first = true;
+            for (String value : line.split("\\s+")) {
+                values[i].setValue(value);
+                i++;
+            }
+
+            counter_ = 0;
+            counter.setValue(counter_);
+        } catch (FileNotFoundException ex) {
+            getModel().getRuntime().handle(ex);
+        } catch (IOException ex) {
+            getModel().getRuntime().handle(ex);
+        }
+    }
+
     @Override
     public void run() {
         super.run();
@@ -149,6 +185,16 @@ public class TemporalContext extends JAMSContext {
                     dataTracer.setOutput(false);
                 }
             }
+        }
+    }
+
+    @Override
+    public void cleanup() {
+        super.cleanup();
+        try {
+            fileReader.close();
+        } catch (IOException ex) {
+            getModel().getRuntime().handle(ex);
         }
     }
 
@@ -169,39 +215,49 @@ public class TemporalContext extends JAMSContext {
             // if yes, return standard enumerator
             return new ComponentEnumerator() {
 
-                ComponentEnumerator ce = getTCChildrenEnumerator();
+                ComponentEnumerator ce = getFICChildrenEnumerator();
                 //DataTracer dataTracers = getDataTracer();
 
                 @Override
                 public boolean hasNext() {
-                    boolean nextTime = current.before(lastValue);
+
                     boolean nextComp = ce.hasNext();
-                    return (nextTime || nextComp);
+                    if (!nextComp) {
+                        try {
+                            line = fileReader.readLine();
+                        } catch (IOException ex) {
+                            getModel().getRuntime().handle(ex);
+                        }
+                    }
+                    boolean nextLine = (line != null);
+                    return (nextLine || nextComp);
                 }
 
                 @Override
                 public boolean hasPrevious() {
-                    boolean prevTime = current.after(timeInterval.getStart());
+                    boolean prevLine = false;
                     boolean prevComp = ce.hasPrevious();
-                    return (prevTime || prevComp);
+                    return (prevLine || prevComp);
                 }
 
                 @Override
                 public Component next() {
                     // check end of component elements list, if required switch to the next
                     // timestep start with the new Component list again
-                    if (!ce.hasNext() && current.before(lastValue)) {
+                    if (!ce.hasNext() && (line != null)) {
                         for (DataTracer dataTracer : getDataTracers()) {
                             dataTracer.trace();
                         }
-                        current.add(timeInterval.getTimeUnit(), timeInterval.getTimeUnitCount());
-                        if (printTime.getValue() > 0) {
-                            if ((counter % printTime.getValue()) == 0) {
-                                counter = 0;
-                                getModel().getRuntime().println(getInstanceName() + " " + current, JAMS.SILENT);
-                            }
-                            counter++;
+
+                        int i = 0;
+                        boolean first = true;
+                        for (String value : line.split("\\s+")) {
+                            values[i].setValue(value);
+                            i++;
                         }
+                        counter_++;
+                        counter.setValue(counter_);
+
                         ce.reset();
                     }
                     return ce.next();
@@ -209,7 +265,7 @@ public class TemporalContext extends JAMSContext {
 
                 @Override
                 public void reset() {
-                    current.setValue(timeInterval.getStart().getValue());
+                    initFile();
                     ce.reset();
                 }
 
@@ -217,11 +273,7 @@ public class TemporalContext extends JAMSContext {
                     if (ce.hasPrevious()) {
                         return ce.previous();
                     } else {
-                        current.add(timeInterval.getTimeUnit(), -timeInterval.getTimeUnitCount());
-                        while (ce.hasNext()) {
-                            ce.next();
-                        }
-                        return ce.previous();
+                        return null;
                     }
                 }
             };
@@ -256,22 +308,22 @@ public class TemporalContext extends JAMSContext {
             };
         }
     }
-    
-    private DataTracer[] getDataTracers() {
-        return dataTracers; 
-    }
-    
-    private ComponentEnumerator getTCChildrenEnumerator() {
+
+    private ComponentEnumerator getFICChildrenEnumerator() {
         return getChildrenEnumerator();
+    }
+
+    private DataTracer[] getDataTracers() {
+        return dataTracers;
     }
 
     @Override
     public long getNumberOfIterations() {
-        return timeInterval.getNumberOfTimesteps();
+        return 1;
     }
 
     @Override
     public String getTraceMark() {
-        return current.toString();
+        return Integer.toString(counter_);
     }
 }
--- JAMScomponents/src/jams/components/gui/SimpleXYPlot.java
+++ JAMScomponents/src/jams/components/gui/SimpleXYPlot.java
@@ -19,7 +19,6 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 package jams.components.gui;
 
 import java.awt.BorderLayout;
@@ -36,6 +35,8 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import jams.model.JAMSGUIComponent;
 import jams.model.JAMSVarDescription;
 import jams.data.*;
+import jams.model.JAMSComponentDescription;
+import jams.model.VersionComments;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -45,50 +46,60 @@ import org.jfree.data.xy.*;
  *
  * @author S. Kralisch
  */
+@JAMSComponentDescription(
+        title = "SimpleXYPlot",
+        author = "Sven Kralisch",
+        description = "This component creates a graphical plot of x/y data, "
+        + "e.g. evaptranspiration plotted against temperature.",
+        date = "2018-09-14",
+        version = "1.0_1"
+)
+@VersionComments(entries = {
+    @VersionComments.Entry(version = "1.0_0", comment = "Initial version"),
+    @VersionComments.Entry(version = "1.0_1", date = "2018-09-14", comment = "Some improvements")
+})
 public class SimpleXYPlot extends JAMSGUIComponent {
-    
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "Title string for plot"
-            )
-            public Attribute.String plotTitle;                              
-    
+            description = "Title string for plot",
+            defaultValue = "Plot Title"
+    )
+    public Attribute.String plotTitle;
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "Title string for x axis"
-            )
-            public Attribute.String xAxisTitle;
-    
+            description = "Title string for x axis",
+            defaultValue = "x axis"
+    )
+    public Attribute.String xAxisTitle;
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "Title string for x axis"
-            )
-            public Attribute.String yAxisTitle;
-    
+            description = "Title string for y axis",
+            defaultValue = "y axis"
+    )
+    public Attribute.String yAxisTitle;
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "Title string for x axis"
-            )
-            public Attribute.Double xValue;
-  
+            description = "Values on x axis"
+    )
+    public Attribute.Double xValue;
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "Title string for x axis"
-            )
-            public Attribute.Double[] yValue;
-                                   
+            description = "Values on y axis"
+    )
+    public Attribute.Double[] yValue;
+
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
-            description = "Flag for disabling or enabling the plot"
-            )
-            public Attribute.Boolean paint;
-    
-    @JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.READ,
-            description = "Flag for disabling or enabling the plot"
-            )
-            public Attribute.Integer PlotCount;
-    
+            description = "Flag for disabling or enabling the plot",
+            defaultValue = "true"
+    )
+    public Attribute.Boolean paint;
+
     transient private XYPlot plot;
     transient private XYSeries dataset[];
     transient private JPanel panel;
@@ -96,7 +107,7 @@ public class SimpleXYPlot extends JAMSGUIComponent {
     transient JButton saveButton;
     int i, graphCountLeft = 0, graphCountRight = 0;
     HashMap<Integer, Color> colorTable = new HashMap<Integer, Color>();
-        
+
     public SimpleXYPlot() {
         colorTable.put(0, Color.red);
         colorTable.put(1, Color.blue);
@@ -109,69 +120,70 @@ public class SimpleXYPlot extends JAMSGUIComponent {
         colorTable.put(8, Color.gray);
         colorTable.put(9, Color.lightGray);
         colorTable.put(10, Color.black);
-	
-	panel = null;		
+
+        panel = null;
     }
-    
-    private void initDataSets(){
-        
-        dataset = new XYSeries[this.PlotCount.getValue()];
-                
-        for (int i=0;i<this.PlotCount.getValue();i++) {
+
+    private void initDataSets() {
+
+        dataset = new XYSeries[yValue.length];
+
+        for (int i = 0; i < dataset.length; i++) {
             XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
             dataset[i] = new XYSeries(plotTitle.getValue());
             plot.setDataset(i, new XYSeriesCollection(dataset[i]));
             renderer.setSeriesLinesVisible(0, true);
-            renderer.setSeriesShapesVisible(0, false);        
-            
+            renderer.setSeriesShapesVisible(0, false);
+
             renderer.setSeriesPaint(0, colorTable.get(i));
             //renderer.set
-            plot.setRenderer(i, renderer);                        
-        }           	
+            plot.setRenderer(i, renderer);
+        }
 
     }
-    private JPanel createPanel() {
-	plot = new XYPlot();
-	plot.setDomainAxis(new NumberAxis(xAxisTitle.getValue()));
-	plot.setRangeAxis(new NumberAxis(yAxisTitle.getValue()));
-        initDataSets();
-	chart = new JFreeChart(plot);
-                                		
-	ChartPanel chartPanel = new ChartPanel(chart, true);
 
-	panel = new JPanel(new BorderLayout());		    		    	    
-	panel.add(chartPanel, BorderLayout.CENTER);	   
-	    
+    private JPanel createPanel() {
+        plot = new XYPlot();
+        plot.setDomainAxis(new NumberAxis(xAxisTitle.getValue()));
+        plot.setRangeAxis(new NumberAxis(yAxisTitle.getValue()));
+//        initDataSets();
+        chart = new JFreeChart(plot);
+
+        ChartPanel chartPanel = new ChartPanel(chart, true);
+
+        panel = new JPanel(new BorderLayout());
+        panel.add(chartPanel, BorderLayout.CENTER);
+
         chart.getPlot().setBackgroundPaint(Color.white);
         chart.getXYPlot().setDomainGridlinePaint(Color.black);
-        
-	return panel;
+
+        return panel;
     }
-    
-    public JPanel getPanel() {  
-	if(panel==null) {
-	    return createPanel();
-	}
-	return panel;
+
+    public JPanel getPanel() {
+        if (panel == null) {
+            return createPanel();
+        }
+        return panel;
     }
-              
+
     @Override
     public void init() {
-    	
+        initDataSets();
     }
-    
+
     @Override
     public void run() {
         if (dataset == null) {
             this.initDataSets();
         }
-    	if(this.paint == null || this.paint.getValue()){
-            for (int i=0;i<dataset.length;i++){
-                dataset[i].add(this.xValue.getValue(),this.yValue[i].getValue());
+        if (this.paint == null || this.paint.getValue()) {
+            for (int i = 0; i < dataset.length; i++) {
+                dataset[i].add(this.xValue.getValue(), this.yValue[i].getValue());
             }
-    	}
+        }
     }
-    
+
     @Override
     public void cleanup() {
 //        saveButton.setEnabled(true);
@@ -182,13 +194,13 @@ public class SimpleXYPlot extends JAMSGUIComponent {
 
         createPanel();
 
-        for (int i=0;i<dataset.length;i++){
+        for (int i = 0; i < dataset.length; i++) {
             int n = objIn.readInt();
-            for (int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 XYDataItem item = dataset[i].getDataItem(j);
                 double x = objIn.readDouble();
                 double y = objIn.readDouble();
-                dataset[i].add(x,y);
+                dataset[i].add(x, y);
             }
         }
     }
@@ -196,10 +208,10 @@ public class SimpleXYPlot extends JAMSGUIComponent {
     private void writeObject(ObjectOutputStream objOut) throws IOException {
         objOut.defaultWriteObject();
 
-        for (int i=0;i<dataset.length;i++){
+        for (int i = 0; i < dataset.length; i++) {
             int n = dataset[i].getItemCount();
             objOut.writeInt(n);
-            for (int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 XYDataItem item = dataset[i].getDataItem(j);
                 objOut.writeDouble(item.getX().doubleValue());
                 objOut.writeDouble(item.getY().doubleValue());
