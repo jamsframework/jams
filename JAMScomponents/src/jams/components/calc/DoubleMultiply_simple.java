8a50ea03eaa09de5867362973b84cfa8fbe612f4
Manfred Fink <manfred.fink@uni-jena.de>
2018-10-24 07:19:38 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3457 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

---
+++ JAMScomponents/src/jams/components/calc/DoubleMultiply_simple.java
@@ -0,0 +1,102 @@
+/*
+ * DoubleMultiply.java
+ * Created on 11.07.2018, 15:51:19
+ *
+ * This file is part of JAMS
+ * Copyright (C) FSU Jena
+ *
+ * JAMS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * JAMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JAMS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package jams.components.calc;
+
+import jams.data.*;
+import jams.model.*;
+
+
+
+/**
+ *
+ * @author Sven Kralisch <sven.kralisch at uni-jena.de>
+ */
+@JAMSComponentDescription(
+        title = "DoubleMultiply",
+        author = "Sven Kralisch",
+        description = "Multiply two double values and return the result",
+        date = "2018-07-11",
+        version = "1.0_0"
+)
+@VersionComments(entries = {
+    @VersionComments.Entry(version = "1.0_0", comment = "Initial version")
+})
+public class DoubleMultiply_simple extends JAMSComponent  {
+
+    /*
+     *  Component attributes
+     */
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "First operand"
+    )
+    public Attribute.Double[] d1;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ,
+            description = "Second operand"
+    )
+    public Attribute.Double[] d2;
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "Result of d1*d2 (element-wise)"
+    )
+    public Attribute.Double[] result;
+
+   
+
+    /*
+     *  Component run stages
+     */
+    //@Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void run() {
+       if (d1.length == d2.length) {
+
+            
+                //@Override
+               
+                    for (int i = 0; i < d1.length; i++) {
+                        result[i].setValue(d1[i].getValue() * d2[i].getValue());
+                    }
+               
+            
+
+        } else if (d2.length == 1) {
+
+            
+                    for (int i = 0; i < d1.length; i++) {
+                        result[i].setValue(d1[i].getValue() * d2[0].getValue());
+                    
+                
+            };
+
+        } else {
+
+            getModel().getRuntime().sendHalt("Attribute d2 has wrong length, should be 1 or length of d1");
+
+        }
+    }
+}
