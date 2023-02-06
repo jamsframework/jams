acf0cbee7611ff97dc8f74414c5980ad5919081f
Sven Kralisch <sven@kralisch.com>
2023-02-06 16:17:27 +0000

    git-svn-id: http://svn.geogr.uni-jena.de/svn/jams/trunk@3593 cee23a32-52fb-0310-bcb1-fe94ad5b3aa2

--- JAMScomponents/src/jams/components/indices/ClimateIndicesForest.java
+++ JAMScomponents/src/jams/components/indices/ClimateIndicesForest.java
@@ -4,6 +4,7 @@
  */
 package jams.components.indices;
 
+import jams.JAMS;
 import jams.data.Attribute;
 import jams.model.JAMSComponent;
 import jams.model.JAMSComponentDescription;
@@ -18,211 +19,143 @@ import jams.model.JAMSVarDescription;
         author = "Christian Fischer",
         description = "Calculated standard Klimakennwerte for J2000Klima")
 public class ClimateIndicesForest extends JAMSComponent {
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "Current time")
-    public Attribute.Calendar time;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "daily min temperatur",
-    unit="°C")
-    public Attribute.Double tmin;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "daily mean temperatur",
-    unit="°C")
-    public Attribute.Double tmean;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "daily max temperatur",
-    unit="°C")
-    public Attribute.Double tmax;
 
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "daily precipitation",
-    unit="L")
+            description = "Current time")
+    public Attribute.Calendar time;
+
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
+            description = "daily mean temperatur",
+            unit = "°C")
+    public Attribute.Double tmean;
+
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
+            description = "daily precipitation",
+            unit = "L")
     public Attribute.Double precip;
-    
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "daily windspeed",
-    unit="m/s")
-    public Attribute.Double wind;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "relative humidity",
-    unit="-")
-    public Attribute.Double relHum;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "daily pot. evapotranspiration",
-    unit="L")
+            description = "daily pot. evapotranspiration",
+            unit = "L")
     public Attribute.Double potET;
-    
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
-    description = "size of hru",
-    unit="m²")
+            description = "area of unit",
+            unit = "m²")
     public Attribute.Double area;
-          
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-    description = "calculates if we are in the forest vegetation period")
-    public Attribute.Double isForestVegetationPeriodTrigger;
-                    
+            description = "current length of the forest vegetation period")
+    public Attribute.Double forestVegetationPeriodLength;
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-    description = "calculates if a dry period has started")
-    public Attribute.Double isBeginningOfDryPeriod;
-    
+            description = "start day of the forest vegetation period")
+    public Attribute.Double forestVegetationPeriodStart;
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-    description = "calculates if we are within a dry period")
-    public Attribute.Double isDryPeriod;
-    
+            description = "end day of the forest vegetation period")
+    public Attribute.Double forestVegetationPeriodEnd;
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-    description = "calculates if more than 20mm precip did occur")
-    public Attribute.Double isPrecipHigher20mm;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-    description = "calculates if more than 30mm precip did occur")
-    public Attribute.Double isPrecipHigher30mm;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-    description = "calculates if more than 40mm precip did occur")
-    public Attribute.Double isPrecipHigher40mm;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
-    description = "calculates if more than 50mm precip did occur")
-    public Attribute.Double isPrecipHigher50mm;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READWRITE,
-    description = "calculates the number of successive days where precip is zero")
-    public Attribute.Double successiveDaysWithoutRain;
-    
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READWRITE,
-    description = "calculates the climatic water balance (P-potET)")
+            description = "calculates the climatic water balance (P-potET)",
+            unit = "mm")
     public Attribute.Double KWB;
 
-    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READWRITE,
-    description = "calculates the klimatische wasserbilanz (P-potET) during forstlicher vegetation period")
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
+            description = "calculates the klimatische wasserbilanz (P-potET) during forest vegetation period",
+            unit = "mm")
     public Attribute.Double KWBinForestVegetationPeriod;
-        
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.READWRITE,
-    description = "tmp variables")
+            description = "tmp variables")
     public Attribute.DoubleArray tmp;
-    
-    private final int INDEX_successiveDaysWithTmeanAboveFiveDegree = 0;
-    private final int INDEX_successiveDaysWithTmeanAboveTenDegree = 1;
-    private final int INDEX_successiveDaysWithTmeanBelowFiveDegree = 2;
-    private final int INDEX_successiveDaysWithTmeanBelowTenDegree = 3;
-    private final int INDEX_KWB_in_forest_vegetation_period = 5;
-    private final int INDEX_SUMMERINDEX = 7;
-    private final int INDEX_WINTERINDEX = 8;
-    private final int INDEX_KWB_window_position = 9;
-    private final int INDEX_KWB_window = 10;    
-    private final int KWB_WINDOW_SIZE = 30;
-    private final int INDEX_SIZE = INDEX_KWB_window+KWB_WINDOW_SIZE;
-    
+
+    private final int INDEX_successiveDaysWithTmeanAboveTenDegree = 0;
+    private final int INDEX_successiveDaysWithTmeanBelowTenDegree = 1;
+    private final int INDEX_KWB_in_forest_vegetation_period = 2;
+    private final int INDEX_KWB_in_forest_vegetation_period_old = 3;
+    private final int INDEX_FVB_end_day = 4;
+    private final int INDEX_SIZE = INDEX_FVB_end_day + 1;
+
     @Override
-    public void run(){
+    public void run() {
+
         int day = time.get(Attribute.Calendar.DAY_OF_YEAR);
-        
-        double tmean = this.tmean.getValue();
-                
-        isDryPeriod.setValue(0);
-        isBeginningOfDryPeriod.setValue(0);
-        isPrecipHigher20mm.setValue(0.0);
-        isPrecipHigher30mm.setValue(0.0);
-        isPrecipHigher40mm.setValue(0.0);
-        isPrecipHigher50mm.setValue(0.0);        
-        
-        if (tmp.getValue() == null){
+
+        if (day == 109) {
+            System.out.println("");
+        }
+
+        if (tmp.getValue() == null) {
             tmp.setValue(new double[INDEX_SIZE]);
         }
-        double inTmp[] = tmp.getValue();
-                                                                                        
-        //Vegetationsperioden
-        if (tmean > 5.0){
-            inTmp[INDEX_successiveDaysWithTmeanAboveFiveDegree]+=1.0;
-            inTmp[INDEX_successiveDaysWithTmeanBelowFiveDegree]=0.0;
-        }else{
-            inTmp[INDEX_successiveDaysWithTmeanBelowFiveDegree]+=1.0;
-            inTmp[INDEX_successiveDaysWithTmeanAboveFiveDegree]=0;
-        }
-        
-        if (tmean >= 10.0){
-            inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree]+=1;
-            inTmp[INDEX_successiveDaysWithTmeanBelowTenDegree]=0;
-        }else{
-            inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree]=0;
-            inTmp[INDEX_successiveDaysWithTmeanBelowTenDegree]+=1;
-        }                        
+        double[] inTmp = tmp.getValue();
 
-        if (inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree] >= 5){
-            isForestVegetationPeriodTrigger.setValue(1.0);
-        }else if (inTmp[INDEX_successiveDaysWithTmeanBelowTenDegree] >= 6 && day >180){
-            isForestVegetationPeriodTrigger.setValue(0.0);
-        }
-        
-        int p = (int)inTmp[INDEX_KWB_window_position];
-        inTmp[INDEX_KWB_window+p] = tmean;
-        p = (p+1)%KWB_WINDOW_SIZE;
-        double tSum = 0;
-        for (int i=0;i<KWB_WINDOW_SIZE;i++){
-            tSum += inTmp[INDEX_KWB_window_position+i];
-        }
-
-        inTmp[INDEX_KWB_window_position] = p;
-        
-        //Niederschlagskenntage
-        if (precip != null) {            
-            //Starkniederschläge
-            double precip_mm = precip.getValue() / area.getValue();
-            if (precip_mm >= 20) {
-                isPrecipHigher20mm.setValue(1.0);
-            }
-            if (precip_mm >= 30) {
-                isPrecipHigher30mm.setValue(1.0);
-            }
-            if (precip_mm >= 40) {
-                isPrecipHigher40mm.setValue(1.0);
-            }
-            if (precip_mm >= 50) {
-                isPrecipHigher50mm.setValue(1.0);
-            }
-            
-            //Trockenperioden
-            if (precip_mm < 0.1) {
-                successiveDaysWithoutRain.setValue(successiveDaysWithoutRain.getValue() + 1);
-
-                if (successiveDaysWithoutRain.getValue() == 11.0) {
-                    isDryPeriod.setValue(11.0);
-                    isBeginningOfDryPeriod.setValue(1.0);
-                } else if (successiveDaysWithoutRain.getValue() > 11.0) {
-                    isDryPeriod.setValue(1.0);
-
-                }
-            } else {
-                successiveDaysWithoutRain.setValue(0.0);
-            }
-                        
-        }
-        
-        //Klimatische Wasserbilanz
-        if (potET != null && precip != null){
-            double KWB_mm = (precip.getValue() - potET.getValue());
-            
-            KWB.setValue(KWB_mm);
-            
-            inTmp[INDEX_KWB_in_forest_vegetation_period] += KWB_mm;
-            if (isForestVegetationPeriodTrigger.getValue()==1){
-                KWBinForestVegetationPeriod.setValue(KWBinForestVegetationPeriod.getValue()+inTmp[INDEX_KWB_in_forest_vegetation_period]);
-                inTmp[INDEX_KWB_in_forest_vegetation_period] = 0;
-            }            
-        }        
-        //reset counters .. 
-        if (time.get(Attribute.Calendar.DAY_OF_YEAR)==1){
+        if (day == 1) {
+            forestVegetationPeriodStart.setValue(0);
+            forestVegetationPeriodEnd.setValue(0);
+            forestVegetationPeriodLength.setValue(0);
             KWBinForestVegetationPeriod.setValue(0);
             inTmp[INDEX_KWB_in_forest_vegetation_period] = 0;
-            inTmp[INDEX_SUMMERINDEX] = -1;
-        }                
-        if (time.get(Attribute.Calendar.DAY_OF_YEAR)==180){
-            inTmp[INDEX_WINTERINDEX] = -1;
         }
+
+        //Vegetationsperioden
+        if (tmean.getValue() >= 10.0) {
+            inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree] += 1;
+//            inTmp[INDEX_successiveDaysWithTmeanBelowTenDegree] = 0;
+        } else {
+            //tmean<10° and 5+-day-period above 10° before
+            if (inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree] >= 5) {
+                inTmp[INDEX_KWB_in_forest_vegetation_period_old] = inTmp[INDEX_KWB_in_forest_vegetation_period];
+                inTmp[INDEX_FVB_end_day] = day - 1;
+            }
+            inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree] = 0;
+//            inTmp[INDEX_successiveDaysWithTmeanBelowTenDegree] += 1;
+
+            //vor Start der FVP?
+            if (forestVegetationPeriodStart.getValue() == 0) {
+                inTmp[INDEX_KWB_in_forest_vegetation_period] = 0;
+            }
+        }
+
+        if (inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree] == 5
+                && forestVegetationPeriodStart.getValue() == 0) {
+            forestVegetationPeriodStart.setValue(day - 4);
+        }
+
+        //wenn außerhalb FVP, alten Wert speichern
+        if (inTmp[INDEX_successiveDaysWithTmeanBelowTenDegree] == 1) {
+//            storeValues(inTmp, day);
+            inTmp[INDEX_KWB_in_forest_vegetation_period_old] = inTmp[INDEX_KWB_in_forest_vegetation_period];
+            inTmp[INDEX_FVB_end_day] = day - 1;
+        }
+
+        //Klimatische Wasserbilanz
+        double KWB_mm = (precip.getValue() - potET.getValue()) / area.getValue();
+        KWB.setValue(KWB_mm);
+
+        //nach Start der FVP?
+//        if (forestVegetationPeriodStart.getValue() != 0) {
+            inTmp[INDEX_KWB_in_forest_vegetation_period] += KWB_mm;
+//        }
+
+        //reset counters .. 
+        if (day >= 365) {
+            if (inTmp[INDEX_successiveDaysWithTmeanAboveTenDegree] >= 5) {
+//                storeValues(inTmp, day);
+                inTmp[INDEX_KWB_in_forest_vegetation_period_old] = inTmp[INDEX_KWB_in_forest_vegetation_period];
+                inTmp[INDEX_FVB_end_day] = day - 1;
+            }
+            KWBinForestVegetationPeriod.setValue(inTmp[INDEX_KWB_in_forest_vegetation_period_old]);
+            forestVegetationPeriodEnd.setValue(inTmp[INDEX_FVB_end_day]);
+            forestVegetationPeriodLength.setValue(forestVegetationPeriodEnd.getValue() - forestVegetationPeriodStart.getValue() + 1);
+            inTmp[INDEX_KWB_in_forest_vegetation_period] = 0;
+        }
+
     }
+
+//    private void storeValues(double[] inTmp, int day) {
+//        inTmp[INDEX_KWB_in_forest_vegetation_period_old] = inTmp[INDEX_KWB_in_forest_vegetation_period];
+//        inTmp[INDEX_FVB_end_day] = day - 1;
+//    }
 }
